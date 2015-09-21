bc9d9e788a7008b2d0da60f1fc0a3d6920c66247
Konstantin Chizhov <kchizhov@mirantis.com>
2015-09-21 14:23:20 +0300

    Add compatibility stuff

---
+++ NplotCompact/Compatibility/DrawingExtensions.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+using System.Drawing;
+
+namespace NPlot
+{
+    public static class DrawingExtensions
+    {
+        static public Brush GetBrush(this Pen pen)
+        {
+            return new SolidBrush(pen.Color);
+        }
+
+        static public void DrawLine(this Graphics g, Pen pen, Point p1, Point p2)
+        {
+            g.DrawLine(pen, p1.X, p1.Y, p2.X, p2.Y);
+        }
+
+        static public void DrawLine(this Graphics g, Pen pen, PointF p1, PointF p2)
+        {
+            g.DrawLine(pen, (int)p1.X, (int)p1.Y, (int)p2.X, (int)p2.Y);
+        }
+
+        static public void DrawLine(this Graphics g, Pen pen, float x1, float y1, float x2, float y2)
+        {
+            g.DrawLine(pen, (int)x1, (int)y1, (int)x2, (int)y2);
+        }
+
+        static public void FillRectangle(this Graphics g, Brush brush, float x, float y, float width, float height)
+        {
+            g.FillRectangle(brush, (int)x, (int)y, (int)width, (int)height);
+        }
+
+        static public void DrawRectangle(this Graphics g, Pen pen, float x, float y, float width, float height)
+        {
+            g.DrawRectangle(pen, (int)x, (int)y, (int)width, (int)height);
+        }
+
+        static public void FillPolygon(this Graphics g, Brush brush, PointF[] pointsf)
+        {
+            Point[] points = new Point[pointsf.Length];
+            for (int i = 0; i < pointsf.Length; i++)
+            {
+                points[i].X = (int)pointsf[i].X;
+                points[i].Y = (int)pointsf[i].Y;
+            }
+            g.FillPolygon(brush, points);
+        }
+
+        static public void DrawPolygon(this Graphics g, Pen pen, PointF[] pointsf)
+        {
+            Point[] points = new Point[pointsf.Length];
+            for (int i = 0; i < pointsf.Length; i++)
+            {
+                points[i].X = (int)pointsf[i].X;
+                points[i].Y = (int)pointsf[i].Y;
+            }
+            g.DrawPolygon(pen, points);
+        }
+
+        static public Brush Clone(this Brush brush)
+        {
+            //return new SolidBrush(((SolidBrush)brush).Color);
+            return brush;
+        }
+
+        static public int GetHeight(this Font font)
+        {
+            return (int)(font.Size * 72 / 96);
+        }
+    }
+}
---
+++ NplotCompact/Compatibility/PointF.cs
@@ -0,0 +1,123 @@
+﻿namespace System.Drawing
+{
+    public struct PointF
+    {
+        private float x, y;
+
+        public static readonly PointF Empty;
+
+        public static implicit operator PointF(Point point)
+        {
+            return new PointF(point.X, point.Y);
+        }
+
+        public static PointF operator +(PointF pt, Size sz)
+        {
+            return new PointF(pt.X + sz.Width, pt.Y + sz.Height);
+        }
+
+        public static PointF operator +(PointF pt, SizeF sz)
+        {
+            return new PointF(pt.X + sz.Width, pt.Y + sz.Height);
+        }
+
+        public static bool operator ==(PointF left, PointF right)
+        {
+            return ((left.X == right.X) && (left.Y == right.Y));
+        }
+
+        public static bool operator !=(PointF left, PointF right)
+        {
+            return ((left.X != right.X) || (left.Y != right.Y));
+        }
+
+        public static PointF operator -(PointF pt, Size sz)
+        {
+            return new PointF(pt.X - sz.Width, pt.Y - sz.Height);
+        }
+
+        public static PointF operator -(PointF pt, SizeF sz)
+        {
+            return new PointF(pt.X - sz.Width, pt.Y - sz.Height);
+        }
+
+
+        public PointF(float x, float y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public bool IsEmpty
+        {
+            get
+            {
+                return ((x == 0.0) && (y == 0.0));
+            }
+        }
+
+
+        public float X
+        {
+            get
+            {
+                return x;
+            }
+            set
+            {
+                x = value;
+            }
+        }
+
+        public float Y
+        {
+            get
+            {
+                return y;
+            }
+            set
+            {
+                y = value;
+            }
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (!(obj is PointF))
+                return false;
+
+            return (this == (PointF)obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return (int)x ^ (int)y;
+        }
+
+        public override string ToString()
+        {
+            return String.Format("{{X={0}, Y={1}}}", x.ToString(),
+                y.ToString());
+        }
+
+        public static PointF Add(PointF pt, Size sz)
+        {
+            return new PointF(pt.X + sz.Width, pt.Y + sz.Height);
+        }
+
+        public static PointF Add(PointF pt, SizeF sz)
+        {
+            return new PointF(pt.X + sz.Width, pt.Y + sz.Height);
+        }
+
+        public static PointF Subtract(PointF pt, Size sz)
+        {
+            return new PointF(pt.X - sz.Width, pt.Y - sz.Height);
+        }
+
+        public static PointF Subtract(PointF pt, SizeF sz)
+        {
+            return new PointF(pt.X - sz.Width, pt.Y - sz.Height);
+        }
+    }
+}
